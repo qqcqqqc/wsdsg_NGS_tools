7f71be6f0aaee90a56d8ff0c655b0f6a27eb4a0a
qqcqqqc <qqcqqqc@users.noreply.github.com>
2026-07-27 00:06:38 +0800

    style: 😛😜🤪😝 极简终端回显：单行展示4个鬼脸 Emoji

--- 双击启动软件.bat
+++ 双击启动软件.bat
@@ -1,20 +1,11 @@
 ﻿@echo off
 chcp 65001 >nul
-title 宇宙无敌NGS tool-cql定制版
+title 😜 宇宙无敌NGS tool-cql定制版
 
 cd /d "%~dp0"
 
 echo.
-echo         .----------------.
-echo        ^|   (==戴夫==)   ^|
-echo         '----------------'
-echo          /  o     O  \
-echo         ^|     ___     ^|
-echo          \   \___/   /
-echo           '---\P/---'
-echo     ==========================
-echo      数据不要慌，戴夫带你飞！
-echo     ==========================
+echo 😛 😜 🤪 😝 正在启动 宇宙无敌NGS tool-cql定制版... 🚀
 echo.
 
 python -m pip install -r requirements.txt -q
