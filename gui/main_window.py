97aea32628a8631c081d618a127a5efb09f99a63
qqcqqqc <qqcqqqc@users.noreply.github.com>
2026-09-22 12:57:08 +0800

    chore(release): bump version to v2.4.0

--- gui/main_window.py
+++ gui/main_window.py
@@ -14,7 +14,7 @@ from gui.tab_demux import DemuxTab
 from gui.tab_crispresso import CRISPRessoTab
 from core.env_checker import check_environment
 
-VERSION = "v2.3.3"
+VERSION = "v2.4.0"
 
 class EnvCheckThread(QThread):
     """
